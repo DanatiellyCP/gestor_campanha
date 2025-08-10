d78d79aa85c42032ee182b143ccd3f4fa337de82
Valmor Júnior <valmor@valmor.com>
2025-08-10 16:15:57 -0300

    review pages to promotion

--- promocaobombril/urls.py
+++ promocaobombril/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.participe, name='participe'),
+    path('como-participar/', views.como_participar, name='como_participar'),
+    path('produtos/', views.produtos, name='produtos'),
+    path('lista-resultado/', views.lista_resultado, name='lista_resultado'),
+    path('cadastrar/', views.cadastrar, name='cadastrar'),
+    path('duvidas/', views.duvidas, name='duvidas'),
+    path('logar/', views.logar, name='logar'),
 ]
--- promocaobombril/views.py
+++ promocaobombril/views.py
@@ -1,5 +1,57 @@
-from django.shortcuts import render
+import datetime
+from participantes.models import Participantes
+from django.shortcuts import render, redirect
+from django.contrib.auth.hashers import make_password, check_password
+import re
+from participantes.forms import ParticipanteForm
+
+from datetime import datetime
 
 def participe(request):
     # lógica da view
     return render(request, 'participe.html')
+
+
+
+def como_participar(request):    
+    return render(request, 'como-participar.html')
+
+def produtos(request):    
+    return render(request, 'produtos.html')
+    
+def lista_resultado(request):    
+    return render(request, 'lista-resultados.html')
+
+def duvidas(request):    
+    return render(request, 'duvidas.html')
+
+def cadastrar(request):
+    if request.method == 'POST':
+        form = ParticipanteForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('logar')
+    else:
+        form = ParticipanteForm()
+    
+    return render(request, 'cadastrar.html', {'form': form})
+
+def logar(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
+
+        try:
+            participante = Participantes.objects.get(email=email)
+            if check_password(senha, participante.senha):
+                # Autenticado com sucesso – salvar ID na sessão
+                request.session['participante_id'] = participante.id
+                return redirect('painel_participante')  # ou a página pós-login
+            else:
+                erro = "Senha incorreta."
+        except Participantes.DoesNotExist:
+            erro = "Participante não encontrado."
+
+        return render(request, 'logar.html', {'erro': erro})
+
+    return render(request, 'logar.html')
