76c43a4c2ad250abd227206069ce06acdfec531c
Valmor Júnior <valmor@valmor.com>
2025-08-11 15:18:13 -0300

    review pages to promotion v1

--- promocaobombril/views.py
+++ promocaobombril/views.py
@@ -25,33 +25,8 @@ def lista_resultado(request):
 def duvidas(request):    
     return render(request, 'duvidas.html')
 
-def cadastrar(request):
-    if request.method == 'POST':
-        form = ParticipanteForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('logar')
-    else:
-        form = ParticipanteForm()
-    
-    return render(request, 'cadastrar.html', {'form': form})
+def cadastrar(request):    
+    return render(request, 'cadastrar.html')
 
 def logar(request):
-    if request.method == 'POST':
-        email = request.POST.get('email')
-        senha = request.POST.get('senha')
-
-        try:
-            participante = Participantes.objects.get(email=email)
-            if check_password(senha, participante.senha):
-                # Autenticado com sucesso – salvar ID na sessão
-                request.session['participante_id'] = participante.id
-                return redirect('painel_participante')  # ou a página pós-login
-            else:
-                erro = "Senha incorreta."
-        except Participantes.DoesNotExist:
-            erro = "Participante não encontrado."
-
-        return render(request, 'logar.html', {'erro': erro})
-
     return render(request, 'logar.html')
